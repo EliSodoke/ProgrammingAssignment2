a9d67ef6b86295d0c3525544ca01cb9ec3bb9399
Eli Sodoke <elisodoke@gmail.com>
2015-04-25 13:46:18 -0400

    Version1

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  inverse<-NULL
+  
+  set<-function(mat)
+  {
+    x <<- mat
+    inverse <<- NULL
+  }
+  
+  get<-function() x;
+  setInverse <- function(inverseValue) inverse <<- inverseValue
+  getInverse <- function() inverse
+  
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
@@ -12,4 +28,27 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  solved <- x$getInverse()
+  
+  
+  if(!is.null(solved)) {
+    message("getting cached data")
+    return(solved)
+  }
+  
+  Mat <- x$get()
+  solved <- solve(Mat)
+  x$setInverse(solved)
+  solved
+  
 }
+
+## Test caching
+## The value of can be changes to generae bigger matrix
+n <- 3
+mat <- matrix(rnorm(1:(n*n)), nrow=n, ncol=n)
+matCached <- makeCacheMatrix(mat)
+matSolved1 <- cacheSolve(matCached)
+matSolved2 <- matCached$getInverse()
+if (!identical(matSolved1, matSolved2)) message("Cached version does not match solved version")
